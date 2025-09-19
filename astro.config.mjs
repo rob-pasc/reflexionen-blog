0cc975be325e53c16fcea8b40b42057f051d4e54
rob-pasc <robinpascal.k@gmail.com>
2025-09-19 17:19:45 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
-
+// // @ts-check
 // import { defineConfig } from 'astro/config';
-// export default defineConfig({
-//   site: 'https://<dein-username>.github.io',
-//   base: '/reflexionen-blog', // NUR für Project Pages!
-// });
+
+// // https://astro.build/config
+// export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+  site: 'https://rob-pasc.github.io',
+  base: '/reflexionen-blog', // NUR für Project Pages!
+});
